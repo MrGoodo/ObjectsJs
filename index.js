74dcb9355dc55aa67b24226c14dabae838f9402c
Nick Kandelaki <goodo.nick@gmail.com>
2022-09-15 12:08:20 +0300

    ObjectsJs

---
+++ index.js
@@ -0,0 +1,13 @@
+//объект и его свойства//
+
+const Person = {
+  Nationality: "Georgian",
+  city: "Tbilisi",
+  sex: "preferably with women",
+  age: 26,
+  Profession: "unemployed",
+  Name: "JoJo",
+  stand: "Star Platinum",
+};
+
+//если я хочу получить доступ к значению какому либо из свойств данного объекта, то нужно ввести console.log(Person.и после точки ввести какое свойство мне нужно, и консоль вернет мне уже значение свойства)//
